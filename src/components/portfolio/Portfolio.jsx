8e7ecec4bd1ed14e3a652a39274fdced3b172afb
Alex Thomas <alexbthomas03@gmail.com>
2023-08-08 18:56:20 -0500

    Working on Portfolio section

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,7 +3,51 @@ import './portfolio.css'
 
 function Portfolio() {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Projects</h2>
+      <div className="container portfolio__container">
+
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            {/* <img src={IMG1} alt=""/> */}
+          </div>
+          <h3>Chat Bot</h3>
+          {/* NLTK */}
+          <a href = "https://github.com" className='btn'>Github</a>
+        </article>
+
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            {/* <img src={IMG1} alt=""/> */}
+          </div>
+          {/* Pytorch */}
+          <h3>Image Classifier</h3>
+          <a href = "https://github.com" className='btn'>Github</a>
+        </article>
+
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            {/* <img src={IMG1} alt=""/> */}
+          </div>
+          {/* Django and mySQL */}
+          <h3>Healthcare Management System</h3>
+          <a href = "https://github.com" className='btn'>Github</a>
+        </article>
+
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            {/* <img src={IMG1} alt=""/> */}
+          </div>
+          {/* React */}
+          <h3>Personal Website</h3>
+          <a href = "https://github.com" className='btn'>Github</a>
+        </article>
+
+        {/* AWS project potentially */}
+      </div>
+    </section>
+    
   )
 }
 
